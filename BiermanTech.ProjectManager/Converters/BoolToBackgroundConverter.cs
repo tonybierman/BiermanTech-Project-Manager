0289e4b43b3bee2c7dc5d2494dfd1a83c97a944e
tonybierman <tonybierman@gmail.com>
2025-04-01 17:20:38 -0500

    Added columns for start and duration

---
+++ BiermanTech.ProjectManager/Converters/BoolToBackgroundConverter.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
+
+namespace BiermanTech.ProjectManager.Converters;
+
+public class BoolToBackgroundConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool isHovering && isHovering)
+        {
+            return new SolidColorBrush(Colors.LightGray);
+        }
+        return Brushes.Transparent;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- BiermanTech.ProjectManager/Services/GanttChartConfig.cs
+++ BiermanTech.ProjectManager/Services/GanttChartConfig.cs
@@ -2,8 +2,8 @@
 
 public static class GanttChartConfig
 {
-    public const double TaskListWidth = 200;
-    public const double HeaderHeight = 40; // This should be enough for two rows (20 + 20)
+    public const double TaskListWidth = 330; // Updated to match total column width (150 + 100 + 80)
+    public const double HeaderHeight = 40;
     public const double MinPixelsPerDay = 10;
     public const double MinRowHeight = 30;
 }
